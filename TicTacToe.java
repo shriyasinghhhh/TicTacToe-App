b92ba2035043fed4d95a4c7801930233ef33bf1a
SHRIYA <shriyasinghhhh@gmail.com>
2026-04-21 13:14:01 +0530

    UC4: Convert slot to row and column

---
+++ TicTacToe.java
@@ -0,0 +1,22 @@
+class TicTacToe {
+
+    // Method to convert slot to row & column
+    static int[] convertSlot(int slot) {
+
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+
+        return new int[]{row, col};
+    }
+
+    public static void main(String[] args) {
+
+        int slot = 5; // example input
+
+        int[] position = convertSlot(slot);
+
+        System.out.println("Slot: " + slot);
+        System.out.println("Row: " + position[0]);
+        System.out.println("Column: " + position[1]);
+    }
+}
